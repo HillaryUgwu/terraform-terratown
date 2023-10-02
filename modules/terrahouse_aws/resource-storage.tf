972b9a718db9c09d26ad00f6ee30c455e2c37ed0
Hillary Ugwu <ugwuhillaryemeka@gmail.com>
2023-10-02 21:25:11 +0200

    #33 feat: only upload index.html if a content version has change (#35)

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -29,6 +29,11 @@ resource "aws_s3_object" "index_html" {
   content_type = "text/html"
 
   etag = filemd5(var.index_html_filepath)
+
+  lifecycle {
+    replace_triggered_by = [terraform_data.content_version.output]
+    ignore_changes = [etag]
+  }
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
@@ -39,7 +44,13 @@ resource "aws_s3_object" "error_html" {
   content_type = "text/html"
 
   etag = filemd5(var.error_html_filepath)
+
+  lifecycle {
+    replace_triggered_by = [terraform_data.content_version.output]
+    ignore_changes = [etag]
+  }
 }
+
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = aws_s3_bucket.website_bucket.bucket
@@ -62,4 +73,8 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
       }
     }
   })
+}
+
+resource "terraform_data" "content_version" {
+  input = var.content_version
 }
